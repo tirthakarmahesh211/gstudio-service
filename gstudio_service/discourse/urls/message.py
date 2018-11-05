77488a1ce69462732f0958cc0709472e3288d98f
Siddhu Dhangar <siddhudhangar@gmail.com>
2018-11-05 16:47:37 +0530

    two urls are added for updating and deleting messages

--- gstudio_service/discourse/urls/message.py
+++ gstudio_service/discourse/urls/message.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
-from discourse.views.message import get_messages, create_message, get_private_messages, get_some_notifications, enable_private_messages
+from discourse.views.message import get_messages, create_message, get_private_messages, get_some_notifications, enable_private_messages, update_message \
+,delete_message
 
 urlpatterns = [
 
@@ -8,4 +9,6 @@ urlpatterns = [
     path('get_private_messages/', get_private_messages, name="get_private_messages"),
     path('get_some_notifications/', get_some_notifications, name="get_some_notifications"),
     path('enable_private_messages/', enable_private_messages, name="enable_private_messages"),
+    path('update_message/', update_message, name="update_message"),
+    path('delete_message/', delete_message, name="delete_message"),
 ]
