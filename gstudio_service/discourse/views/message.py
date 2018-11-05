a5184bc96f0549af7b3b2f32673659036641aafc
Siddhu Dhangar <siddhudhangar@gmail.com>
2018-11-05 16:53:33 +0530

    created separate message.py file into views directory

--- gstudio_service/discourse/views/message.py
+++ gstudio_service/discourse/views/message.py
@@ -1,34 +1,56 @@
 from django.shortcuts import render
-from discourse.discourse import Discourse
 from django.http import HttpResponse
-from gstudio_service.local_settings import DISCOURSE_URL, DISCOURSE_USERNAME, DISCOURSE_API_KEY
+from discourse.models.message import Message
+import json
 
 # Create your views here.
-api = Discourse(DISCOURSE_URL, DISCOURSE_USERNAME, DISCOURSE_API_KEY)
-
+params = {}
 def get_messages(request):
-	return HttpResponse(api.get_messages({"before":40},"message"))
+	params = {"before":40}
+	# return HttpResponse(api.get_messages({"before":40},"message"))
+	return HttpResponse(json.dumps(Message(**params).get_messages()), content_type="application/json")	
 
 def get_private_messages(request):
-	return HttpResponse(api.get_private_messages({"username":"username"},"message"))
+	# return HttpResponse(api.get_private_messages({"username":"username"},"message"))
+	return HttpResponse(json.dumps(Message(**params).get_private_messages()), content_type="application/json")	
 
 def enable_private_messages(request):
-	return HttpResponse(api.enable_private_messages({"enable_private_messages":"true" },"message"))
+	# return HttpResponse(api.enable_private_messages({"enable_private_messages":"true" },"message"))
+	return HttpResponse(json.dumps(Message(**params).enable_private_messages()), content_type="application/json")	
 
 def create_message(request):
-	return HttpResponse(api.create_message({
-	"title": "Test messageemmmmm for private_message",
-    # "topic_id": 36,
-    "raw": "test message",
-    # "category": 0,
-    "target_usernames": "@TestGroup2",
-    # "archetype": "private_message",
-    # "created_at": "2017-01-31"
-    },"message"))
+	if request.POST:
+		title = request.POST.get("title")
+		raw = request.POST.get("raw")
+		target_usernames = request.POST.get("target_usernames")
+		archetype = "private_message"
+		params = {"title":title,"raw":raw,"target_usernames":target_usernames,"archetype":archetype}
 
+	else:
+		params = {"title":"Test message vvv rrrrrrrrrggggggggggg pppp","raw": "private test messae mmmmmmmmmmmmmmm","target_usernames":"NewGroup10","archetype": "private_message"}
+	return HttpResponse(json.dumps(Message(**params).create_message()), content_type="application/json")
 
 def get_some_notifications(request):
-	return HttpResponse(api.get_some_notifications({},"message"))
+	return HttpResponse(json.dumps(Message(**params).get_some_notifications()), content_type="application/json")
 
-def reply_for_messages(request):
-	return HttpResponse(api.reply_for_messages({"title":"Reply message","topic_id":18},"message"))
+def reply_message(request):
+	params = {"title": "reply message vvv rrrrrrrrrggggggggggg pppp","topic_id":108 }
+	return HttpResponse(json.dumps(Message(**params).reply_message()), content_type="application/json")
+	# return HttpResponse(reply_for_messages({"title":"Reply message","topic_id":18},"message"))
+
+def update_message(request):
+	if request.POST:
+		id = request.POST.get("id")
+		raw = request.POST.get("raw")
+		params = {"post[raw]":raw}
+	else:
+		params = {"title":"Test message vvv rrrrrrrrrggggggggggg pppp","post[raw]":"Test messageeeerrrrrrrrrrrrrrrrrr"}
+	return HttpResponse(json.dumps(Message(**params).update_message()), content_type="application/json")
+
+def delete_message(request):
+	if request.POST:
+		title = request.POST.get("title")
+		params = {"title":title}
+	else:
+		params = {"title":"Test message vvv rrrrrrrrrggggggggggg pppp"}
+	return HttpResponse(json.dumps(Message(**params).delete_message()), content_type="application/json")
