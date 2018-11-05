87b4434f81b03df0aaa9e47c6db1d1d0a98fc457
Siddhu Dhangar <siddhudhangar@gmail.com>
2018-11-05 16:54:00 +0530

    created separate page.py file into views directory

--- gstudio_service/discourse/views/page.py
+++ gstudio_service/discourse/views/page.py
@@ -1,54 +1,48 @@
 from django.shortcuts import render
-from discourse.discourse import Discourse
 from django.http import HttpResponse
-from gstudio_service.local_settings import DISCOURSE_URL, DISCOURSE_USERNAME, DISCOURSE_API_KEY
-from django.views.decorators.csrf import csrf_exempt
-# Create your views here.
-api = Discourse(DISCOURSE_URL, DISCOURSE_USERNAME, DISCOURSE_API_KEY)
-
+from discourse.models.page import Page
+import json
 
 def add_page(request):
 	if request.POST:
 		if request.POST.get("title") and request.POST.get("raw"):
 			title = request.POST.get("title")
 			raw = request.POST.get("raw")
-			SAMPLE_JSON = {"title":title,"raw":raw}
+			params = {"title":title,"raw":raw}
 		else:
 			return HttpResponse("Failed")
 	else:
-		SAMPLE_JSON = {"title":"New Page Through API","raw":"New Page Description Through API"}
-	return HttpResponse(api.add_page(SAMPLE_JSON,"page"))
+		params = {"title":"New Page Through APIIIIIIIIIIIIIIIII","raw":"New Page Description Through API"}
+	return HttpResponse(json.dumps(Page(**params).add_page()), content_type="application/json")
 
 def update_page(request):
 	if request.POST:
 		if request.POST.get("title") and request.POST.get("id"):
 			title = request.POST.get("title")
+			new_page_title = request.POST.get("new_page_title")
 			id = request.POST.get("id")
-			SAMPLE_JSON = {"title":title,"id":id }
+			params = {"title":title,"id":id }
 		else:
 			return HttpResponse("Failed")
 	else:
-		SAMPLE_JSON = {"title":"New page title11","id":17,"category_id": 0}
-	return HttpResponse(api.update_page(SAMPLE_JSON,"page"))
+		params = {"title":"new page title11","new_page_title":"new page title22"}
+	return HttpResponse(json.dumps(Page(**params).update_page()), content_type="application/json")	
 
 def delete_page(request):
 	if request.POST:
-		if request.POST.get("id"):
-			id = request.POST.get("id")
-			SAMPLE_JSON = {"id":id}
-		else:
-			return HttpResponse("Failed")
+		title = request.POST.get("title")
+		params = {"title":title,"category_id": 0}
 	else:
-		SAMPLE_JSON = {"id":17,"category_id": 0}
-	return HttpResponse(api.delete_page(SAMPLE_JSON,"page"))
+		params = {"title":"Test messageemmmmm for private message","category_id": 0}
+	return HttpResponse(json.dumps(Page(**params).delete_page()), content_type="application/json")	
 
 def get_page(request):
 	if request.GET:
 		if request.GET.get("id"):
 			id = request.GET.get("id")
-			SAMPLE_JSON = {"id":id}
+			params = {"id":id}
 		else:
 			return HttpResponse("Failed")
 	else:
-		SAMPLE_JSON = {"id":49,"category_id": 0}
-	return HttpResponse(api.get_page(SAMPLE_JSON,"page"))
+		params = {"id":78}
+	return HttpResponse(json.dumps(Page(**params).get_page()), content_type="application/json")	
