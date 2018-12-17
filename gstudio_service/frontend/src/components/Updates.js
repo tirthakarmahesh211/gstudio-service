04d5208a8443550f608f15376e37ee96a3e6e8ca
tirthakarmahesh211 <tirthakarmahesh211@gmail.com>
2018-12-17 12:07:49 +0000

    Content added in card body

--- gstudio_service/frontend/src/components/Updates.js
+++ gstudio_service/frontend/src/components/Updates.js
@@ -16,7 +16,11 @@ class Updates extends Component {
     						Updates
     					</div>
     					<div className="card-body">
-    					
+    						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vulputate risus justo, non accumsan urna semper ut. Quisque consequat consectetur nisl, pellentesque vulputate purus egestas a. Sed fermentum arcu turpis, at faucibus lectus sagittis sit amet. Donec lacinia consequat velit, at consequat odio. Ut sed tortor eu lorem fringilla semper at ac mi. Aliquam bibendum tellus et dui tempor tincidunt. Proin ultrices, est non eleifend porttitor, purus justo pulvinar urna, vitae semper neque elit non sapien.
+
+Vivamus accumsan turpis quis magna feugiat, a facilisis libero laoreet. Sed mauris magna, bibendum in nunc cursus, ornare tempor diam. Integer quis volutpat eros. Suspendisse lacinia lacinia purus, tincidunt dapibus velit malesuada a. Vestibulum sit amet nisi gravida, porttitor odio sed, cursus ligula. Fusce vestibulum libero a quam consectetur, quis mattis turpis vehicula. Nunc molestie odio nec purus iaculis, vitae gravida dui placerat. Morbi ut ipsum vitae tellus accumsan rhoncus.
+
+Phasellus non felis iaculis, fringilla mi quis, aliquam nisl. Integer dapibus lorem risus, ac venenatis tortor convallis at. Fusce sit amet quam vitae augue aliquet lobortis et a nibh. Suspendisse vel purus fringilla ante ornare euismod. Nam porttitor, augue condimentum efficitur dapibus, mauris metus viverra nibh, vitae egestas dolor lectus at sem. Donec dictum lorem nunc, et rhoncus lorem consectetur nec. Etiam velit libero, dictum sed nisi vel, fringilla eleifend nibh. Etiam interdum urna in nibh facilisis, eget vehicula sapien imperdiet. Duis velit neque, posuere non ipsum eu, fermentum fermentum mauris.
     					</div> 
     					<div className="card-footer">
     						footer
@@ -29,6 +33,7 @@ class Updates extends Component {
     						Upadtes Desc
     					</div>
     					<div className="card-body">
+    						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vulputate risus justo, non accumsan urna semper ut. Quisque consequat consectetur nisl, pellentesque vulputate purus egestas a. Sed fermentum arcu turpis, at faucibus lectus sagittis sit amet. Donec lacinia consequat velit, at consequat odio. Ut sed tortor eu lorem fringilla semper at ac mi. Aliquam bibendum tellus et dui tempor tincidunt. Proin ultrices, est non eleifend porttitor, purus justo pulvinar urna, vitae semper neque elit non sapien.
     					</div>
  
     					<div className="card-footer">
--- gstudio_service/frontend/src/components/groups/Group_index.js
+++ gstudio_service/frontend/src/components/groups/Group_index.js
@@ -37,7 +37,11 @@ class Group_index extends Component {
     						Groups
     					</div>
     					<div className="card-body">
-    					
+    						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vulputate risus justo, non accumsan urna semper ut. Quisque consequat consectetur nisl, pellentesque vulputate purus egestas a. Sed fermentum arcu turpis, at faucibus lectus sagittis sit amet. Donec lacinia consequat velit, at consequat odio. Ut sed tortor eu lorem fringilla semper at ac mi. Aliquam bibendum tellus et dui tempor tincidunt. Proin ultrices, est non eleifend porttitor, purus justo pulvinar urna, vitae semper neque elit non sapien.
+
+Vivamus accumsan turpis quis magna feugiat, a facilisis libero laoreet. Sed mauris magna, bibendum in nunc cursus, ornare tempor diam. Integer quis volutpat eros. Suspendisse lacinia lacinia purus, tincidunt dapibus velit malesuada a. Vestibulum sit amet nisi gravida, porttitor odio sed, cursus ligula. Fusce vestibulum libero a quam consectetur, quis mattis turpis vehicula. Nunc molestie odio nec purus iaculis, vitae gravida dui placerat. Morbi ut ipsum vitae tellus accumsan rhoncus.
+
+Phasellus non felis iaculis, fringilla mi quis, aliquam nisl. Integer dapibus lorem risus, ac venenatis tortor convallis at. Fusce sit amet quam vitae augue aliquet lobortis et a nibh. Suspendisse vel purus fringilla ante ornare euismod. Nam porttitor, augue condimentum efficitur dapibus, mauris metus viverra nibh, vitae egestas dolor lectus at sem. Donec dictum lorem nunc, et rhoncus lorem consectetur nec. Etiam velit libero, dictum sed nisi vel, fringilla eleifend nibh. Etiam interdum urna in nibh facilisis, eget vehicula sapien imperdiet. Duis velit neque, posuere non ipsum eu, fermentum fermentum mauris.
     					</div> 
     					<div className="card-footer">
     						footer
@@ -50,6 +54,8 @@ class Group_index extends Component {
     						Group Detail
     					</div>
     					<div className="card-body">
+    						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vulputate risus justo, non accumsan urna semper ut. Quisque consequat consectetur nisl, pellentesque vulputate purus egestas a. Sed fermentum arcu turpis, at faucibus lectus sagittis sit amet. Donec lacinia consequat velit, at consequat odio. Ut sed tortor eu lorem fringilla semper at ac mi. Aliquam bibendum tellus et dui tempor tincidunt. Proin ultrices, est non eleifend porttitor, purus justo pulvinar urna, vitae semper neque elit non sapien.
+
     					</div>
  
     					<div className="card-footer">
