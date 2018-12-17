8d55bcf1ac93fd0b72f48eaae28e533ed1d63a77
amitmoralwar <amitmoralwar@gmail.com>
2018-12-17 10:41:31 +0000

    Added icons in the header for note,message,notification,bookmark,badge,profile,grid

--- gstudio_service/frontend/src/components/Navigation.js
+++ gstudio_service/frontend/src/components/Navigation.js
@@ -1,4 +1,12 @@
 import React, {Component} from 'react';
+import note from  './../icons/note.png';
+import message from './../icons/message.png';
+import bookmark from './../icons/bookmark.png';
+import notification from './../icons/notification.png';
+import badge from './../icons/badge.png';
+import avatar from './../icons/avatar.png';
+import metastudio from './../icons/metastudiologo.png';
+import grid from './../icons/9grid.png';
 
 class Navigation extends Component {
 
@@ -12,7 +20,7 @@ class Navigation extends Component {
 
             <nav id="sidebar">
             <div className="sidebar-header">
-                <h3>Metastudio</h3>
+                <img src ={metastudio} width="245"/>
             </div>
 
 
@@ -91,24 +99,38 @@ class Navigation extends Component {
                         <span></span>
                         <span></span>
                     </button>
+                      <div class="grid-button">
+                        <a className="nav-link" href="#"><img src ={grid} alt="" width="30" height="35"/></a>
+                        </div>
+  
                     <button className="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <i className="fas fa-align-justify"></i>
                     </button>
 
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="nav navbar-nav ml-auto">
+
                             <li className="nav-item active">
-                                <a className="nav-link" href="#">Page</a>
+                                <a className="nav-link" href="#"><img src ={note} alt="" width="30" height="35"/></a>
+                            </li>
+
+                            <li className="nav-item">
+                                <a className="nav-link" href="#"><img src ={message} alt="" width="30" height="35"/></a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Page</a>
+                                <a className="nav-link" href="#"><img src ={bookmark} alt="" width="30" height="35"/></a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Page</a>
+                                <a className="nav-link" href="#"><img src ={notification} alt="" width="30" height="35"/></a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Page</a>
+                                <a className="nav-link" href="#"><img src ={badge} alt="" width="30" height="35"/></a>
                             </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#"><img src ={avatar} alt="" width="30" height="35"/></a>
+                            </li>
+
+                            
                         </ul>
                     </div>
                 </div>
