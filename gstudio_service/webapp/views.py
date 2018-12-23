767fc9b32c7fc6d26f6980340cc990df184c6aa0
Siddhu Dhangar <siddhudhangar@gmail.com>
2018-12-23 12:52:34 +0000

    get_pages method added, for getting all activity pages from particular group

--- gstudio_service/webapp/views.py
+++ gstudio_service/webapp/views.py
@@ -3,7 +3,7 @@ import requests
 from gstudio_service.settings import HOSTNAME
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
-
+import json
 # Create your views here.
 @csrf_exempt
 def create_or_update_page(request):
@@ -34,10 +34,30 @@ def create_or_update_page(request):
 def get_pages(request):
 	if request.GET:
 		group_id = request.GET.get("group_id")
+		api_call = request.GET.get("api_call")
 
 	elif request.POST:
 		group_id = request.POST.get("group_id")
+		api_call = request.GET.get("api_call")
 
 	URL = HOSTNAME + group_id + "/course/activities/"
-	response = requests.post(URL,data={})
+	response = requests.post(URL,data={"api_call":api_call})
+	return HttpResponse(response,content_type="application/json")
+
+@csrf_exempt
+def delete_page(request):
+	if request.GET:
+		delete_files_list = request.GET.getlist('delete_files_list[]', '')
+		activity_id = request.GET.get('activity_id', '')
+		group_id = request.GET.get("group_id"),
+		username = request.GET.get("username")
+
+	elif request.POST:
+		delete_files_list = request.POST.getlist('delete_files_list[]', '')
+		activity_id = request.POST.get('activity_id', '')
+		group_id = request.POST.get("group_id"),
+		username = request.POST.get("username")
+
+	URL = HOSTNAME + group_id + "/course/delete_activity_page/"
+	response = requests.post(URL,data={"username":username,"group_id":group_id,"delete_files_list":delete_files_list,"activity_id":activity_id})
 	return HttpResponse(response,content_type="application/json")
