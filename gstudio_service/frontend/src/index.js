d0a297d674a85742bd3b9bcfc9796d86178b25fe
tirthakarmahesh211 <tirthakarmahesh211@gmail.com>
2018-12-26 12:32:37 +0000

    New packages added in package.json

--- gstudio_service/frontend/src/index.js
+++ gstudio_service/frontend/src/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import 'babel-polyfill';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-    import { BrowserRouter } from 'react-router-dom';
+//import registerServiceWorker from './registerServiceWorker';
+import { HashRouter } from 'react-router-dom'
+import ScrollToTop from './ScrollToTop';
 
-import 'bootstrap/dist/css/bootstrap.css';
+ReactDOM.render(
+    <HashRouter>
+        <ScrollToTop>
+        	<BrowserRouter>
+    <App />
+    </BrowserRouter>
+        </ScrollToTop>
+    </HashRouter>,
+    document.getElementById('root')
+);
 
-
-ReactDOM.render((
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>
-	)
-	,document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//registerServiceWorker();
